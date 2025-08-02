62b54d43a256395626ab1f6d76b5ae34f1c2e449
Joan Salazar <joansal95@gmail.com>
2025-08-02 14:09:14 -0500

    Fix routing configuration: move BrowserRouter to main.jsx with corrected basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Pokemon from './pages/Pokemon'
 
 function App() {
   return (
-    <Router basename="/Pokedexmo">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/pokemon/:id" element={<Pokemon />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/pokemon/:id" element={<Pokemon />} />
+    </Routes>
   );
 }
 
